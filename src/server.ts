984447a7313a0cc8c6b5a95e6775224d6e7f572f
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-05-15 13:34:02 +0900

    add wss

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
-import express from "express";
+import e from "express";
+import { createServer } from "http";
+import { WebSocketServer } from "ws";
 
-const app = express();
+const app = e();
 const PORT = 3000;
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", e.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(PORT, () =>
-  console.log(`Server listening on port: http://localhost:${PORT}`)
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
+
+server.listen(PORT, () =>
+  console.log(`Zoom app listening on port: http://localhost:${PORT} 🚀`)
 );
